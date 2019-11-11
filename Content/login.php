cf57c97d0db070e0991c06d97de2355990401226
CarlRWarren <cwarren@student.neumont.edu>
2019-11-11 11:39:30 -0700

    gage stuff

---
+++ Content/login.php
@@ -0,0 +1,11 @@
+<?php include "header.php"; ?>
+<h1>Login</h1>
+
+<form action="/login.php">
+    Username:
+    <input type="text" name="user">
+    Password:
+    <input type="password" name="pass">
+    <input type="submit" value="Submit">
+</form>
+<?php include "footer.php"; ?>
