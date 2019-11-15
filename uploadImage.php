a3f0eb1a7c129e67819db0662cec16ddedd78510
Verdactel <36209800+Verdactel@users.noreply.github.com>
2019-11-15 10:38:56 -0700

    Update uploadImage.php

--- uploadImage.php
+++ uploadImage.php
@@ -5,8 +5,11 @@
 
     $imagetype = strtolower(pathinfo($newPath, PATHINFO_EXTENSION));
     
-    move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $newPath);
-    
-    echo "<img src=".$newPath.">";
-
+    if(file_exists($newPath)) {
+        echo "<img src=".$newPath.">";
+    }
+    else{
+        move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $newPath);
+        echo "<img src=".$newPath.">";
+    }
 ?>
