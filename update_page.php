f664c050a940c59a666f90b8398e593042f834f7
Verdactel <36209800+Verdactel@users.noreply.github.com>
2019-11-15 19:52:45 -0700

    AJAX SAVING

--- update_page.php
+++ update_page.php
@@ -1,19 +1,33 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         include 'dbconfig.php';
-        $query = "update pages set ";
+        $query = "update pages ";
+
+        $shouldSet = true;
         
         if(isset($_POST['Header'])){
-            $query .= "Header='".$mysqli->real_escape_string($_POST['Header'])."'";
+            if($shouldSet){
+                $query .= "SET Header='".$mysqli->real_escape_string($_POST['Header'])."'";
+                $shouldSet = false;
+            }
+            else{
+                $query .= ",Header='".$mysqli->real_escape_string($_POST['Header'])."'";
+            }
         }
         if(isset($_POST['Content'])){
-            $query .= "Content='".$mysqli->real_escape_string($_POST['Content'])."'";
+            if($shouldSet){
+                $query .= "SET Content='".$mysqli->real_escape_string($_POST['Content'])."'";
+                $shouldSet = false;
+            }
+            else{
+                $query .= ",Content='".$mysqli->real_escape_string($_POST['Content'])."'";
+            }
         }
         // if(isset($_POST['image'])){
         //     $query .= "ImageName='".$mysqli->real_escape_string($_POST['image'])."'";
         // }
         
-        $query .= " Where PAGE_ID=2"; //$mysqli->real_escape_string($_REQUEST['2'])
+        $query .= " WHERE PAGE_ID=2"; //$mysqli->real_escape_string($_REQUEST['2'])
         
         if(! $mysqli->query($query) ) {
             echo "Database Error: Unable to update record.";
